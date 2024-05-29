7e6fd56cd620bf991d050c6ec218bb937aba9a6e
Hala Mohammed <halaalimohammed@gmail.com>
2024-05-29 11:57:39 +0200

    1st commit

---
+++ src/components/accordian/data.js
@@ -0,0 +1,68 @@
+let data = [
+    {
+      
+        id: '1',
+        Question: 'What do you understand by Artificial Intelligence?',
+        Answer: 'Artificial intelligence is computer science technology that emphasizes creating intelligent machine that can mimic human behavior. Here Intelligent machines can be defined as the machine that can behave like a human'
+        +'think like a human, and also capable of decision making. It is made up of two words, "Artificial" and "Intelligence," which means the "man-made thinking ability.'
+        +' With artificial intelligence, we do not need to pre-program the machine to perform a task; instead, we can create a machine with the programmed algorithms, and it can work on its own.'
+       
+    },
+    {
+        id: '2',
+        Question: 'Why do we need Artificial Intelligence?',
+        Answer: 'The goal of Artificial intelligence is to create intelligent machines that can mimic human behavior.'
+        +'We need AI for today´s world to solve complex problems, make our lives more smoothly by automating the routine work,'
+        +' saving the manpower, and to perform many more other tasks'
+       
+    },
+    {
+        id: '3',
+        Question: 'What are the different domains/Subsets of AI?',
+        Answer: '-Machine Learning' + '-Deep Learning' +'-Neural Network' + '-Neural Language Processing' 
+        +'-Expert System' +'-Robotics' +'-Speed Recognition'
+        +' -Fuzzy Logic.'
+       
+    },
+    {
+        id: '4',
+        Question: 'Which programming language is used for AI?',
+        Answer: '-Python' + '-Java' + '-Lisp' + '-R' + '-Prolog'   
+        
+    },
+    {
+        id: '5',
+        Question: 'What do you understand by Artificial Intelligence?',
+        Answer: 'Artificial intelligence is computer science technology that emphasizes creating intelligent machine that can mimic human behavior. Here Intelligent machines can be defined as the machine that can behave like a human'
+        +'think like a human, and also capable of decision making. It is made up of two words, "Artificial" and "Intelligence," which means the "man-made thinking ability.'
+        +' With artificial intelligence, we do not need to pre-program the machine to perform a task; instead, we can create a machine with the programmed algorithms, and it can work on its own.'
+       
+    },
+    {
+        id: '6',
+        Question: 'What do you understand by Artificial Intelligence?',
+        Answer: 'Artificial intelligence is computer science technology that emphasizes creating intelligent machine that can mimic human behavior. Here Intelligent machines can be defined as the machine that can behave like a human'
+        +'think like a human, and also capable of decision making. It is made up of two words, "Artificial" and "Intelligence," which means the "man-made thinking ability.'
+        +' With artificial intelligence, we do not need to pre-program the machine to perform a task; instead, we can create a machine with the programmed algorithms, and it can work on its own.'
+       
+    },
+    {
+        id: '7',
+        Question: 'What do you understand by Artificial Intelligence?',
+        Answer: 'Artificial intelligence is computer science technology that emphasizes creating intelligent machine that can mimic human behavior. Here Intelligent machines can be defined as the machine that can behave like a human'
+        +'think like a human, and also capable of decision making. It is made up of two words, "Artificial" and "Intelligence," which means the "man-made thinking ability.'
+        +' With artificial intelligence, we do not need to pre-program the machine to perform a task; instead, we can create a machine with the programmed algorithms, and it can work on its own.'
+       
+    },
+    {
+        id: '8',
+        Question: 'What do you understand by Artificial Intelligence?',
+        Answer: 'Artificial intelligence is computer science technology that emphasizes creating intelligent machine that can mimic human behavior. Here Intelligent machines can be defined as the machine that can behave like a human'
+        +'think like a human, and also capable of decision making. It is made up of two words, "Artificial" and "Intelligence," which means the "man-made thinking ability.'
+        +' With artificial intelligence, we do not need to pre-program the machine to perform a task; instead, we can create a machine with the programmed algorithms, and it can work on its own.'
+       
+    },
+    
+];
+
+export default data;
